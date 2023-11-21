9be178333ca16243992244b73755adaee2a9fa0a
aescribano <aescribano@cendrassos.net>
2023-11-21 16:43:31 +0100

    Html-Login-1.0

--- App/Views/header.php
+++ App/Views/header.php
@@ -1,9 +1,10 @@
 <header>
     <nav class="bg-red-600 border-gray-200 px-4 lg:px-6 py-2.5">
         <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-            <a href="https://flowbite.com" class="flex items-center">
-                <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
-                <span class="text-white self-center text-xl font-semibold whitespace-nowrap">Flowbite</span>
+            <a href="https://cendrassos.net" class="flex items-center">
+                <div class="bg-white rounded-lg w-44 h-12 flex justify-center items-center">
+                    <img src="/imgs/logo.png" class="mr-3 h-6 sm:h-9" alt="Cendrassos Logo" />
+                </div>
             </a>
             <div class="flex items-center lg:order-2">
                 <a href="#" class="text-white hover:bg-white hover:text-primary-700 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2  focus:outline-none">Log in</a>
