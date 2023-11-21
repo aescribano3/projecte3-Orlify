cec8f7d08bbadcc579de0243c6499cc4da27baa6
aescribano <aescribano@cendrassos.net>
2023-11-21 16:48:30 +0100

    Footer Logo Fix

--- App/Views/footer.php
+++ App/Views/footer.php
@@ -1,9 +1,9 @@
 <footer class="bg-red-600 w-full text-center mt-auto bottom-0">
     <div class="w-full max-w-screen-xl mx-auto  md:py-5">
         <div class="sm:flex sm:items-center sm:justify-between">
-            <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                <img src="/imgs/logo.png" class="h-8" alt="logo web" />
-            </a>
+            <div class="bg-white rounded-lg w-44 h-12 flex justify-center items-center">
+                <img src="/imgs/logo.png" class="mr-3 h-6 sm:h-9" alt="Cendrassos Logo" />
+            </div>
             <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-50 sm:mb-0 dark:text-gray-400">
                 <li>
                     <a href="#" class="hover:underline me-4 md:me-6">Sobre Nosotros</a>
