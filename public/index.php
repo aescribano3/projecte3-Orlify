bf66eb28186b6edaad4beaf0c5629775a4850fa0
aryouch <145044953+aryouch@users.noreply.github.com>
2023-11-22 15:32:12 +0100

    vista y controlador de orla

--- public/index.php
+++ public/index.php
@@ -13,5 +13,6 @@ $app = new \Emeset\Emeset($contenidor);
 
 $app->get("/", "App\Controllers\portada:ctrlPortada");
 $app->get("/login", "App\Controllers\login:ctrlIndex");
-$app->get("carnet", "App\Controllers\carnet:ctrlCarnet"); // llamar al controlador de carnet
+$app->get("/carnet", "App\Controllers\carnet:ctrlIndex"); // llamar al controlador de carnet
+$app->get("/orla", "App\Controllers\orla:ctrlIndex"); // llamar al controlador de orla
 $app->execute();
