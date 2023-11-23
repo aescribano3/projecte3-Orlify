6cd365ff0e864e7247f84ec96595307c33f2bf5c
aryouch <145044953+aryouch@users.noreply.github.com>
2023-11-23 15:16:51 +0100

    fix

---
+++ App/Views/orla.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="/main.css">
+    <link rel="stylesheet" href="/css/style.css">
+    <title>Orla</title>
+</head>
+
+<body class="bg-gray-100 font-sans">
+    <?php include "header.php" ?>
+
+    <div class="mx-auto mt-8 p-4">
+        <h1 class="text-3xl font-bold text-center mb-4">Orla x</h1>
+        
+        <div class="max-w-screen-md mx-auto mt-10 relative bg-slate-300 overflow-hidden">
+            <img class="w-full h-auto object-cover rounded-lg" src="https://orlasomega.es/wp-content/uploads/2020/08/ORLA-CLASICA-2.jpg" alt="Imagen de la orla">
+        </div>
+    </div>
+
+    <script src="/js/flowbite.min.js"></script>
+    <script src="/js/bundle.js"></script>
+    
+    <?php include "footer.php" ?>
+</body>
+
+</html>
