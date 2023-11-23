cc65a15664337257338a717252b33f6959649ec0
larauz17 <snctip@gmail.com>
2023-11-23 15:35:14 +0100

    fix-css

--- public/index.php
+++ public/index.php
@@ -11,23 +11,15 @@ $contenidor = new \App\Container(__DIR__ . "/../App/config.php");
 
 $app = new \Emeset\Emeset($contenidor);
 
-<<<<<<< HEAD
 $app->get("/", "App\Controllers\login:ctrlIndex");
 $app->get("/mis-datos", "App\Controllers\userdata:ctrlIndex");
 $app->get("/register", "App\Controllers\singup:ctrlIndex");
 $app->get("/carnet", "App\Controllers\Carnet:ctrlIndex"); // llamar al controlador de carnet
 $app->get("/view-orla", "App\Controllers\ViewOrla:ctrlIndex"); // llamar al controlador de orla
-=======
-$app->get("/", "App\Controllers\portada:ctrlPortada");
-$app->get("/login", "App\Controllers\login:ctrlIndex");
-$app->get("/carnet", "App\Controllers\user:ctrlCarnet"); // llamar al controlador de carnet
-$app->get("/mis-datos", "App\Controllers\user:ctrlDades");
-$app->get("/imgiorla", "App\Controllers\imguser:ctrlIndex");
-
-
-
->>>>>>> feature-alumne-img-perfil
-$app->get(Router::DEFAULT_ROUTE, "App\Controllers\Error:ctrlIndex");
 $app->get("/info-grup", "App\Controllers\Professors:ctrlIndex");
+
+
+
+$app->get(Router::DEFAULT_ROUTE, "App\Controllers\Error:ctrlIndex");
 
 $app->execute();
