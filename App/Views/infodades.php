fbd44dcb5cbf31e0854e8c495c30564e217b4792
aescribano <aescribano@cendrassos.net>
2023-11-24 18:55:08 +0100

    Fix

--- App/Views/infodades.php
+++ App/Views/infodades.php
@@ -323,14 +323,6 @@
             </div>
         </aside>
 
-<<<<<<< HEAD
-        <!-- Grups Table -->
-        <div id="GrupTable" class="w-full overflow-x-auto shadow-md sm:rounded-lg">
-            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                    <tr>
-                        <th class="px-6 py-4">
-=======
         <!-- User Table -->
         <div id="UserTable" class="w-full overflow-x-auto shadow-md sm:rounded-lg">
             <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
@@ -405,7 +397,6 @@
                 <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
                         <th scope="col" class="px-6 py-4">
->>>>>>> Html-ControlPanel-Users
                             <button data-modal-target="create-grup" data-modal-toggle="create-grup" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
                                 Afegir Grup
                             </button>
