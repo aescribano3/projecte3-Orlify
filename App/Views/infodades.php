c5bea9f2671e8c77393f5a938457d67bc8ad8c1d
larauz17 <snctip@gmail.com>
2023-11-27 15:08:43 +0100

    comentario

--- App/Views/infodades.php
+++ App/Views/infodades.php
@@ -167,8 +167,7 @@
             </table>
         </div>
 
-
-        
+<!-- comentario -->
         <!-- Orles Table -->
         <div id="OrlaTable" class="w-full shadow-md sm:rounded-lg ">
             <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
