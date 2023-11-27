a0866a74bec9cd32621a30864221c29b389e96b3
larauz17 <snctip@gmail.com>
2023-11-27 19:37:31 +0100

    register-finish

--- App/Models/Users.php
+++ App/Models/Users.php
@@ -12,7 +12,7 @@ class Users
     }
 
     public function register($username,$nom,$lastname,$pass,$email,$rol){
-        $insertStmt = $this->sql->prepare('INSERT INTO users (username,name1, lastname, pass, email, rol) VALUES (:username, :name1,  :lastname, :pass, :email, :rol);');
+        $insertStmt = $this->sql->prepare('INSERT INTO users (username,name, lastname, password, email, rol) VALUES (:username, :name1,  :lastname, :pass, :email, :rol);');
         $result = $insertStmt->execute([
             ':username' => $username,
             ':name1' => $nom,
@@ -21,8 +21,19 @@ class Users
             ':email' => $email,
             ':rol' => $rol,
         ]);
-        return "Registro exitoso.";
+        return $lastInsertId = $this->sql->lastInsertId();;
 
     }
 
+
+    public function addphoto($ruta, $id) {
+        $updateStmt = $this->sql->prepare('UPDATE users SET avatar = :ruta WHERE idUser = :user_id');
+        $result = $updateStmt->execute([
+            ':user_id' => $id,
+            ':ruta' => $ruta
+        ]);
+    
+        // Asegúrate de manejar $result según tus necesidades
+        return $result;
+    }
 }
