532e78ab9c4a6aaaf4f0218b7ce1dfc6630b9884
aescribano <aescribano@cendrassos.net>
2023-11-28 19:14:33 +0100

    Fix-Merge

--- App/js/index.js
+++ App/js/index.js
@@ -1,11 +1,6 @@
 import jQuery from "jquery";
-<<<<<<< HEAD
 window.$ = window.jQuery = jQuery;
 
 import "./key.js"
-=======
 
 import "./ajax-grup.js";
-
-window.$ = window.jQuery = jQuery;
->>>>>>> PHP-Create-Grup
