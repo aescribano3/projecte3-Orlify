f9aabde37c3f0797d172eca80acf59bfc1849593
aescribano <aescribano@cendrassos.net>
2023-11-30 15:18:26 +0100

    Fix-Portada

--- App/Controllers/Portada.php
+++ App/Controllers/Portada.php
@@ -7,8 +7,7 @@ class Portada{
 
     public function ctrlIndex($request, $response, $container)
     {
-        $username = $_SESSION["user"]["username"];
-        $response->set("username", $username);
+
         $response->SetTemplate("portada.php");
 
         return $response;
