b02c1859519b52197cf6320ee24af2ae0a7865c3
aescribano <aescribano@cendrassos.net>
2023-12-04 18:01:37 +0100

    Fix-DataTables

--- App/js/datatables.js
+++ App/js/datatables.js
@@ -1,26 +1,63 @@
 import $ from 'jquery';
 import 'datatables.net';
 
+$("#TableUser").DataTable( {
+    "language": {
+        "sSearch": "Cercar:",
+        "lengthMenu": "Mostrar _MENU_ lineas per pagina",
+        "zeroRecords": "No s'ha trobat cap resultat",
+        "info": "Pagina _PAGE_ de _PAGES_",
+        "infoEmpty": "No hi ha registres disponibles",
+        "infoFiltered": "(filtrat d'un total de _MAX_ total registres)",
+        'paginate': {
+            'previous': 'Anterior',
+            'next': 'Seguent'
+    }
+}} );
+
+$("#TableGrup").DataTable( {
+    "language": {
+        "sSearch": "Cercar:",
+        "lengthMenu": "Mostrar _MENU_ lineas per pagina",
+        "zeroRecords": "No s'ha trobat cap resultat",
+        "info": "Pagina _PAGE_ de _PAGES_",
+        "infoEmpty": "No hi ha registres disponibles",
+        "infoFiltered": "(filtrat d'un total de _MAX_ total registres)",
+        'paginate': {
+            'previous': 'Anterior',
+            'next': 'Seguent'
+    }
+}} );
+
+$("#TableOrla").DataTable( {
+    "language": {
+        "sSearch": "Cercar:",
+        "lengthMenu": "Mostrar _MENU_ lineas per pagina",
+        "zeroRecords": "No s'ha trobat cap resultat",
+        "info": "Pagina _PAGE_ de _PAGES_",
+        "infoEmpty": "No hi ha registres disponibles",
+        "infoFiltered": "(filtrat d'un total de _MAX_ total registres)",
+        'paginate': {
+            'previous': 'Anterior',
+            'next': 'Seguent'
+    }
+}} );
+
+
 $('#UsersLink').on('click', function () {
     $('#UserTable').show();
     $('#GrupTable').hide();
     $('#OrlaTable').hide();
-
-    $("#TableUser").DataTable();
 });
 
 $('#GrupsLink').on('click', function () {
     $('#UserTable').hide();
     $('#GrupTable').show();
     $('#OrlaTable').hide();
-
-    $("#TableGrup").DataTable();
 });
 
 $('#OrlesLink').on('click', function () {
     $('#UserTable').hide();
     $('#GrupTable').hide();
     $('#OrlaTable').show();
-
-    $("#TableOrla").DataTable();
 });
