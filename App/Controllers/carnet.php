972d4ea92f1c9f8ad706f19b3b1e2a65b3805fab
aryouch <145044953+aryouch@users.noreply.github.com>
2023-12-14 17:17:40 +0100

    carnet se ve bien con la foto de perfil y los datos de lusuario junto con el grupo y el último curso

--- App/Controllers/carnet.php
+++ App/Controllers/carnet.php
@@ -5,9 +5,21 @@ namespace App\Controllers;
 class Carnet{
 
 
-    public function ctrlIndex($request, $response, $container)
-    {
+    public function ctrlIndex($request, $response, $container) {
+        
+        $grupUserModel = $container->get("grupuser");
+        $user = $_SESSION["user"];
+        $userId = $user["idUser"];
 
+        $infoGrup = $grupUserModel->getidGrup($userId);
+        $idGrup = $infoGrup["idGrup"];
+
+        $info = $grupUserModel->getNameGrup($userId, $idGrup);
+        $grupName = $info["name"];
+        $grupCurs = $info["curs"];
+        
+        $response->set("grupName" , $grupName);
+        $response->set("grupCurs" , $grupCurs);
         $response->SetTemplate("carnet.php");
 
         return $response;
