ac665cabcce4903c45aefa14cea120f96b0ff9b9
larauz17 <snctip@gmail.com>
2023-12-15 17:05:35 +0100

    casi

---
+++ App/Middleware/auth.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Middleware;
+
+class Auth
+{
+
+    /**
+     * Middleware que gestiona l'autenticació
+     *
+     * @param \Emeset\Http\Request $request petició HTTP
+     * @param \Emeset\Http\Response $response resposta HTTP
+     * @param \Emeset\Container $container  
+     * @param callable $next  següent middleware o controlador.   
+     * @return \Emeset\Http\Response resposta HTTP
+     */
+    public static function auth($request, $response, $container, $next)
+    {
+
+        $user = $request->get("SESSION", "user");
+        $logged = $request->get("SESSION", "logged");
+
+        if (!isset($logged)) {
+            $user = "";
+            $logged = false;
+        }
+
+        $response->set("user", $user);
+        $response->set("logged", $logged);
+
+        // si l'usuari està logat permetem carregar el recurs
+        if ($logged) {
+            $response = \Emeset\Middleware::next($request, $response, $container, $next);
+        } else {
+            $response->redirect("location: /login");
+        }
+
+        return $response;
+    }
+}
