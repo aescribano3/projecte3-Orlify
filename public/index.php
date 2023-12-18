05d0674123a6158c21466514490e66306e143068
aescribano <aescribano@cendrassos.net>
2023-12-18 16:04:10 +0100

    Fix-Uppercassse

--- public/index.php
+++ public/index.php
@@ -12,27 +12,27 @@ $app->middleware([\App\Middleware\App::class, "execute"]);
 
 $app->get("/", "App\Controllers\Portada:ctrlIndex");
 $app->get("/login", "App\Controllers\Login:ctrlIndex");
-$app->get("/logout", "App\Controllers\login:ctrlDoLogout");
-$app->post("/dologin", "App\Controllers\login:ctrlDoLogin");
+$app->get("/logout", "App\Controllers\Login:ctrlDoLogout");
+$app->post("/dologin", "App\Controllers\Login:ctrlDoLogin");
 $app->get("/mis-datos", "App\Controllers\User:ctrlmisdades",[[\App\Middleware\Auth::class,"auth"]]);
 $app->get("/register", "App\Controllers\Singup:ctrlIndex");
+$app->post("/doregister", "App\Controllers\Singup:Register");
+
 $app->get("/carnet", "App\Controllers\Carnet:ctrlIndex",[[\App\Middleware\Auth::class,"auth"]]);
 $app->get("/view-orla", "App\Controllers\ViewOrla:ctrlIndex",[[\App\Middleware\Auth::class,"auth"]]);
+$app->post("/checkkey", "App\Controllers\ViewOrla:CheckKey");
+$app->get("/imatges-usuari", "App\Controllers\Imguser:ctrlimatgeuser");
+$app->get("/dadesorla", "App\Controllers\Imguser:ctrlOrla");
+$app->get("/ver-aquesta-orla", "App\Controllers\Imguser:ctrlorlaimg");
+$app->get("/selecionarimatge", "App\Controllers\Imguser:selectimg",[[\App\Middleware\Auth::class,"auth"]]);
+$app->get("/esborrar-img", "App\Controllers\Imguser:deleteimg");
+$app->get("/denegarinformar", "App\Controllers\Imguser:denegarinformar",[[\App\Middleware\Auth::class,"auth"]]);
+$app->get("/informarimatge", "App\Controllers\Imguser:informimg",[[\App\Middleware\Auth::class,"auth"]]);
+$app->post("/afegir-usuari-foto", "App\Controllers\Imguser:afegirimatge",[[\App\Middleware\notalumne::class,"notalumne"]]);
+
 $app->get("/info-dades", "App\Controllers\Controlpanel:ctrlIndex",[[\App\Middleware\notalumne::class,"notalumne"]]);
-$app->get("/imatges-usuari", "App\Controllers\imguser:ctrlimatgeuser");
-$app->get("/dadesorla", "App\Controllers\imguser:ctrlOrla");
-$app->get("/ver-aquesta-orla", "App\Controllers\imguser:ctrlorlaimg");
-$app->get("/selecionarimatge", "App\Controllers\imguser:selectimg",[[\App\Middleware\Auth::class,"auth"]]);
-$app->get("/esborrar-img", "App\Controllers\imguser:deleteimg");
-
-$app->get("/denegarinformar", "App\Controllers\imguser:denegarinformar",[[\App\Middleware\Auth::class,"auth"]]);
-
-$app->get("/informarimatge", "App\Controllers\imguser:informimg",[[\App\Middleware\Auth::class,"auth"]]);
-
-
 $app->post("/getuser", "App\Controllers\Controlpanel:ctrlgetuser",[[\App\Middleware\notalumne::class,"notalumne"]]);
 $app->post("/getgrup", "App\Controllers\Controlpanel:ctrlgetgrup",[[\App\Middleware\notalumne::class,"notalumne"]]);
-$app->post("/doregister", "App\Controllers\singup:Register");
 $app->post("/create-grup", "App\Controllers\Controlpanel:createGrup",[[\App\Middleware\notalumne::class,"notalumne"]]);
 $app->post("/modifi-grup", "App\Controllers\Controlpanel:modifiGrup",[[\App\Middleware\notalumne::class,"notalumne"]]);
 $app->post("/drop-grup", "App\Controllers\Controlpanel:dropGrup",[[\App\Middleware\notalumne::class,"notalumne"]]);
@@ -44,10 +44,8 @@ $app->post("/modifiorla", "App\Controllers\Controlpanel:ctrlmodifiOrla",[[\App\M
 $app->post("/droporla", "App\Controllers\Controlpanel:ctrldropOrla",[[\App\Middleware\notalumne::class,"notalumne"]]);
 $app->post("/updatedatauser", "App\Controllers\User:Updateuser",[[\App\Middleware\Auth::class,"auth"]]);
 $app->post("/checkpass", "App\Controllers\User:checkpass");
-$app->post("/afegir-usuari-foto", "App\Controllers\imguser:afegirimatge",[[\App\Middleware\notalumne::class,"notalumne"]]);
-$app->post("/check-username", "App\Controllers\User:checkuser");
-$app->post("/checkkey", "App\Controllers\ViewOrla:CheckKey");
 
+$app->post("/check-username", "App\Controllers\User:checkuser");
 
 $app->get(Router::DEFAULT_ROUTE, "App\Controllers\Error:ctrlIndex");
 
